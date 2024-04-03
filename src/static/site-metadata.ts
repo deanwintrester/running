95fb2cab7ef7e3ed3812c482c0623f223bcb32ee
Dao <67734164+deanwintrester@users.noreply.github.com>
2024-04-03 13:17:02 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -11,18 +11,18 @@ interface ISiteMetadataResult {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'Running Page',
-  siteUrl: '',
+  siteUrl: 'https://daodl.top',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
   description: 'Personal site and blog',
   navLinks: [
     {
-      name: '',
-      url: '',
+      name: 'Cycling',
+      url: 'https://deanwintrester.github.io/cycling',
     },
-    {
-      name: '',
-      url: '',
-    },
+    // {
+    //   name: '',
+    //   url: '',
+    // },
   ],
 };
 
